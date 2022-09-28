e0b00e354f2719ed8bdbdc8c248acdfc4915190c
MKish420 <meaghanmpeters@hotmail.com>
2022-09-27 21:04:55 -0400

    added firebase tailwinds styling to navbar and user google auth

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration put in env file before deploy 
+const firebaseConfig = {
+  apiKey: "AIzaSyANH89gBSSDGZ0hRt_AhMJ18O9kd5nTft0",
+  authDomain: "travel-nurse-chat-app.firebaseapp.com",
+  projectId: "travel-nurse-chat-app",
+  storageBucket: "travel-nurse-chat-app.appspot.com",
+  messagingSenderId: "440006214470",
+  appId: "1:440006214470:web:b66b1119283b4f121c5b47"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
